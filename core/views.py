945c9e2b6f056959ede7bd4e51f66392d6f4dece
Andrey Isaev <an.isaev7@gmail.com>
2023-10-07 14:59:13 +0300

    add registation/authorization

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from core.forms import RegisterForm
+from core.models import User
+
 
 # Create your views here.
+class RegisterView(CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy("users:login")
